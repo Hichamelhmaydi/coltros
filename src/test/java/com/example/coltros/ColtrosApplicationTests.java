0432e934df1d37fb8189ea7514df53c9f0540f6a
Hichamelhmaydi <ct1594065@gmail.com>
2025-11-21 12:06:42 +0100

    first commit

---
+++ src/test/java/com/example/coltros/ColtrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coltros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColtrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
