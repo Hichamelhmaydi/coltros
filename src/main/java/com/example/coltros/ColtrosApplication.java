89969d923eb94dc1e31b6064defb4df4939e1cbb
Hichamelhmaydi <ct1594065@gmail.com>
2025-12-19 11:07:38 +0100

    update ColtrosApplication

--- src/main/java/com/example/coltros/ColtrosApplication.java
+++ src/main/java/com/example/coltros/ColtrosApplication.java
@@ -2,12 +2,12 @@ package com.example.coltros;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
+@EnableMongoAuditing
 public class ColtrosApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ColtrosApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ColtrosApplication.class, args);
+    }
 }
